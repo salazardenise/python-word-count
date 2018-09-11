7fb998a680609d62daaa0cbe3c8a1ef1d6e9ba61
Denise Salazar <denise.salazar.1210@gmail.com>
2018-09-10 21:50:16 -0700

    Improve output by stripping punctuation and ignoring capitalization

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,20 @@
-# put your code here.
+from string import punctuation
+
+def strip_punctuation(s):
+    """
+    This function strips the string s of punctuation.
+
+    source: https://www.quora.com/How-do-I-remove-punctuation-from-a-Python-string
+    """
+    return ''.join(c for c in s if c not in punctuation)
 
 words_frequency = {}
 
-for line in open("twain.txt"):
-    line = line.rstrip()
-    words = line.split(" ")
+for line in open("test.txt"):
+    words = line.rstrip().lower().split(" ")
     for word in words:
-        words_frequency[word] = words_frequency.get(word, 0) + 1
+        word_stripped = strip_punctuation(word)
+        words_frequency[word_stripped] = words_frequency.get(word_stripped, 0) + 1
 
 for word, count in words_frequency.items():
     print(f'{word} {count}')
